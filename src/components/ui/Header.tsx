2b6e50682e0d11a6cc278497134fbccde3e0b049
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-30 14:53:29 +0300

    Add src/components/ui/Header.tsx

---
+++ src/components/ui/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="bg-blue-600 p-4 text-white">
+      <div className="container mx-auto">
+        <h1 className="text-lg font-bold">Note App</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
